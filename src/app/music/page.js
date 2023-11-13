57698c33d9d44095335a3c6297b2b215faf6a38a
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-11-13 16:15:07 +0000

    spotify player and videos hidden whilst I work out a fix for bugs

--- src/app/music/page.js
+++ src/app/music/page.js
@@ -4,17 +4,44 @@ import { PiTidalLogoFill } from "react-icons/pi"
 export default function Music() {
     return (
             <div className="flex flex-col justify-center items-center mt-10">
-                <video autoPlay muted loop className="w-80 rounded">
+                <h2 className="text-2xl mt-4 uppercase">No Hope</h2>
+                <div className="flex flex-row items-center mb-11">
+                    <FaSpotify size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaApple size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <PiTidalLogoFill size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaDeezer size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaAmazon size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaYoutube size={28} className="hover:scale-110 mx-2 duration-200" />
+                </div>
+                <video autoPlay muted loop className="w-80 rounded hidden lg:block">
                     <source src="/when-i-am-not.mp4" />
                 </video>
                 <h2 className="text-2xl mt-4 uppercase">When I Am Not</h2>
                 <div className="flex flex-row items-center mb-11">
-                <FaSpotify size={28} className="hover:scale-110 mx-2 duration-200" />
-                <FaApple size={28} className="hover:scale-110 mx-2 duration-200" />
-                <PiTidalLogoFill size={28} className="hover:scale-110 mx-2 duration-200" />
-                <FaDeezer size={28} className="hover:scale-110 mx-2 duration-200" />
-                <FaAmazon size={28} className="hover:scale-110 mx-2 duration-200" />
-                <FaYoutube size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaSpotify size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaApple size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <PiTidalLogoFill size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaDeezer size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaAmazon size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaYoutube size={28} className="hover:scale-110 mx-2 duration-200" />
+                </div>
+                <h2 className="text-2xl mt-4 uppercase">Still Existence</h2>
+                <div className="flex flex-row items-center mb-11">
+                    <FaSpotify size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaApple size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <PiTidalLogoFill size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaDeezer size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaAmazon size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaYoutube size={28} className="hover:scale-110 mx-2 duration-200" />
+                </div>
+                <h2 className="text-2xl mt-4 uppercase">Shallow Life</h2>
+                <div className="flex flex-row items-center mb-11">
+                    <FaSpotify size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaApple size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <PiTidalLogoFill size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaDeezer size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaAmazon size={28} className="hover:scale-110 mx-2 duration-200" />
+                    <FaYoutube size={28} className="hover:scale-110 mx-2 duration-200" />
                 </div>
             </div>    
     )
