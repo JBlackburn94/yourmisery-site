4a80207072a6c8b32560f9dd498bb5bfc255d493
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-11-13 18:07:00 +0000

    merch section started

--- src/app/merch/page.js
+++ src/app/merch/page.js
@@ -1,5 +1,36 @@
+import Image from "next/image"
+
 export default function Merch() {
     return (
-        <h1>Merch Page</h1>
+        <div className="flex flex-wrap justify-center items-center">
+            <Image
+                src='/tee-1.webp'
+                height={100}
+                width={400}
+                alt="Your Misery is Mine Tee"
+                className="bg-slate-400 rounded-xl bg-opacity-10 hover:scale-105 duration-200 mb-4 ml-4 w-1/3"
+            />
+            <Image
+                src='/tee-1.webp'
+                height={100}
+                width={400}
+                alt="Your Misery is Mine Tee"
+                className="bg-slate-400 rounded-xl bg-opacity-10 hover:scale-105 duration-200 mb-4 ml-4 w-1/3"
+            />
+            <Image
+                src='/tee-1.webp'
+                height={100}
+                width={400}
+                alt="Your Misery is Mine Tee"
+                className="bg-slate-400 rounded-xl bg-opacity-10 hover:scale-105 duration-200 mb-4 ml-4 w-1/3"
+            />
+            <Image
+                src='/tee-1.webp'
+                height={100}
+                width={400}
+                alt="Your Misery is Mine Tee"
+                className="bg-slate-400 rounded-xl bg-opacity-10 hover:scale-105 duration-200 mb-4 ml-4 w-1/3"
+            />
+        </div>
     )
 }
