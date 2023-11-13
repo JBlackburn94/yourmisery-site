a48de4b6c15b8a02a4a4c27fef03eea0376670ec
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-11-13 15:44:58 +0000

    added layout to the music section

--- src/app/music/page.js
+++ src/app/music/page.js
@@ -3,16 +3,16 @@ import { PiTidalLogoFill } from "react-icons/pi"
 
 export default function Music() {
     return (
-            <div className="flex justify-evenly items-start mt-10">
+            <div className="flex flex-col justify-center items-center mt-10">
                 <video autoPlay muted loop src="/when-i-am-not.mp4" className="w-80 rounded"></video>
-                <div className="flex flex-col items-center">
-                <h2 className="text-2xl uppercase">When I Am Not</h2>
-                <FaSpotify size={40} className="mt-8 hover:scale-110 duration-200" />
-                <FaApple size={40} className="mt-8 hover:scale-110 duration-200" />
-                <PiTidalLogoFill size={40} className="mt-8 hover:scale-110 duration-200" />
-                <FaDeezer size={40} className="mt-8 hover:scale-110 duration-200" />
-                <FaAmazon size={40} className="mt-8 hover:scale-110 duration-200" />
-                <FaYoutube size={40} className="mt-8 hover:scale-110 duration-200" />
+                <h2 className="text-2xl mt-4 uppercase">When I Am Not</h2>
+                <div className="flex flex-row items-center">
+                <FaSpotify size={28} className="hover:scale-110 mx-2 duration-200" />
+                <FaApple size={28} className="hover:scale-110 mx-2 duration-200" />
+                <PiTidalLogoFill size={28} className="hover:scale-110 mx-2 duration-200" />
+                <FaDeezer size={28} className="hover:scale-110 mx-2 duration-200" />
+                <FaAmazon size={28} className="hover:scale-110 mx-2 duration-200" />
+                <FaYoutube size={28} className="hover:scale-110 mx-2 duration-200" />
                 </div>
             </div>    
     )
