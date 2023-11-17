9d42f7f62ea71588ae3584e423aa1c9e8ea6866e
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-11-17 15:25:12 +0000

    added styles to merch page

--- src/app/merch/page.js
+++ src/app/merch/page.js
@@ -2,35 +2,51 @@ import Image from "next/image"
 
 export default function Merch() {
     return (
-        <div className="flex flex-wrap justify-center items-center mt-10">
-            <Image
-                src='/tee-1.webp'
-                height={100}
-                width={400}
-                alt="Your Misery is Mine Tee"
-                className="bg-slate-400 rounded-xl bg-opacity-10 hover:scale-105 duration-200 mb-4 ml-4 w-1/3"
-            />
-            <Image
-                src='/tee-1.webp'
-                height={100}
-                width={400}
-                alt="Your Misery is Mine Tee"
-                className="bg-slate-400 rounded-xl bg-opacity-10 hover:scale-105 duration-200 mb-4 ml-4 w-1/3"
-            />
-            <Image
-                src='/tee-1.webp'
-                height={100}
-                width={400}
-                alt="Your Misery is Mine Tee"
-                className="bg-slate-400 rounded-xl bg-opacity-10 hover:scale-105 duration-200 mb-4 ml-4 w-1/3"
-            />
-            <Image
-                src='/tee-1.webp'
-                height={100}
-                width={400}
-                alt="Your Misery is Mine Tee"
-                className="bg-slate-400 rounded-xl bg-opacity-10 hover:scale-105 duration-200 mb-4 ml-4 w-1/3"
-            />
+        <div className="flex flex-wrap justify-center items-center mt-10 md:flex-col lg:flex-row shrink-0">
+            <div className="bg-slate-400 rounded-xl bg-opacity-40 m-4 flex justify-center items-center flex-col py-2">
+                <Image
+                    src='/tee-1.webp'
+                    height={100}
+                    width={400}
+                    alt="Your Misery is Mine Tee"
+                />
+                <p className="text-center">Is Mine T-Shirt</p>
+                <p className="text-center">£20</p>
+                <button className="bg-black py-2 px-4 rounded hover:scale-105">Add</button>
+            </div>
+            <div className="bg-slate-400 rounded-xl bg-opacity-40 m-4 flex justify-center items-center flex-col py-2">
+                <Image
+                    src='/tee-1.webp'
+                    height={100}
+                    width={400}
+                    alt="Your Misery is Mine Tee"
+                />
+                <p className="text-center">Is Mine T-Shirt</p>
+                <p className="text-center">£20</p>
+                <button className="bg-black py-2 px-4 rounded hover:scale-105">Add</button>
+            </div>
+            <div className="bg-slate-400 rounded-xl bg-opacity-40 m-4 flex justify-center items-center flex-col py-2">
+                <Image
+                    src='/tee-1.webp'
+                    height={100}
+                    width={400}
+                    alt="Your Misery is Mine Tee"
+                />
+                <p className="text-center">Is Mine T-Shirt</p>
+                <p className="text-center">£20</p>
+                <button className="bg-black py-2 px-4 rounded hover:scale-105">Add</button>
+            </div>
+            <div className="bg-slate-400 rounded-xl bg-opacity-40 m-4 flex justify-center items-center flex-col py-2">
+                <Image
+                    src='/tee-1.webp'
+                    height={100}
+                    width={400}
+                    alt="Your Misery is Mine Tee"
+                />
+                <p className="text-center">Is Mine T-Shirt</p>
+                <p className="text-center">£20</p>
+                <button className="bg-black py-2 px-4 rounded hover:scale-105">Add</button>
+            </div>
         </div>
     )
 }
