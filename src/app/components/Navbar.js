0bdf6a4b9554b827d365b65add4a7bb99b122aae
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-12-16 11:34:29 +0000

    fixed navlinks and titles

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -5,74 +5,78 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import Image from "next/image";
 
 export default function Navbar() {
+  const [nav, setNav] = useState(false);
 
-    const [nav, setNav] = useState(false);
+  const links = [
+    {
+      id: 1,
+      link: "/",
+      title: "Home",
+    },
+    {
+      id: 2,
+      link: "merch",
+      title: "Merch",
+    },
+    {
+      id: 3,
+      link: "music",
+      title: "Music",
+    },
+    {
+      id: 4,
+      link: "shows",
+      title: "Shows",
+    },
+  ];
 
-    const links = [
-        {
-            id: 1,
-            link: "home",
-        },
-        {
-            id: 2,
-            link: "merch",
-        },
-        {
-            id: 3,
-            link: "music",
-        },
-        {
-            id: 4,
-            link: "shows",
-        },
-    ];
+  return (
+    <div className="flex items-center justify-between w-full h-20 px-4 text-white bg-black bg-opacity-50 nav">
+      <div>
+        <h1 className="ml-2">
+          <a href="/" rel="noreferrer">
+            <Image
+              src="/logo.png"
+              width={100}
+              height={100}
+              alt="Your Misery logo"
+            />
+          </a>
+        </h1>
+      </div>
 
-    return (
-        <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black bg-opacity-50 nav">
-          <div>
-            {/* <h1 className="text-5xl font-signature ml-2"><a className="link-underline hover:transition ease-in-out delay-150 hover:underline hover:decoration-solid" href="">Logo</a></h1> */}
-            <h1 className="ml-2">
-              <a
-                href="/home"
-                rel="noreferrer"
-              >
-                <Image src="/logo.png" width={100} height={100} alt="Your Misery logo" />
-              </a>
-            </h1>
-          </div>
-    
-          <ul className="hidden md:flex">
-            {links.map(({ id, link }) => (
-              <li
-                key={id}
-                className="nav-links px-4 cursor-pointer capitalize font-medium text-slate-200 hover:scale-105 hover:text-white duration-200 link-underline"
-              >
-                <Link href={link}>{link}</Link>
-              </li>
-            ))}
-          </ul>
-    
-          <div
-            onClick={() => setNav(!nav)}
-            className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      <ul className="hidden md:flex">
+        {links.map(({ id, link, title }) => (
+          <li
+            key={id}
+            className="px-4 font-medium capitalize duration-200 cursor-pointer nav-links text-slate-200 hover:scale-105 hover:text-white link-underline"
           >
-            {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-          </div>
-    
-          {nav && (
-            <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-              {links.map(({ id, link }) => (
-                <li
-                  key={id}
-                  className="px-4 cursor-pointer capitalize py-6 text-4xl"
-                >
-                  <Link onClick={() => setNav(!nav)} href={link}>
-                    {link}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      );
-};
+            <Link href={link}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="z-10 pr-4 text-gray-500 cursor-pointer md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-screen text-gray-500 bg-gradient-to-b from-black to-gray-800">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 py-6 text-4xl capitalize cursor-pointer"
+            >
+              <Link onClick={() => setNav(!nav)} href={link}>
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FaSpotify, FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <p>Home page</p>
-  )
+    <div className="flex flex-col items-center justify-center mt-11">
+      <Image
+        src="/hero-image.webp"
+        height={500}
+        width={1000}
+        alt="Shallow Life Banner"
+        className="w-4/5 rounded-sm"
+      />
+      <p className="mt-5">The New Single</p>
+      <h1 className="text-4xl uppercase">Shallow Life</h1>
+      <button className="px-4 py-2 mt-4 duration-500 rounded bg-slate-700 hover:bg-slate-400">
+        <Link
+          href="https://open.spotify.com/artist/7qJanCPQlXfO9DajSlDR2n"
+          target="_blank"
+        >
+          Listen Now!
+        </Link>
+      </button>
+      <div className="flex mt-4">
+        <Link className="mx-2" href="">
+          <FaSpotify size={28} />
+        </Link>
+        <Link className="mx-2" href="">
+          <FaInstagram size={28} />
+        </Link>
+        <Link className="mx-2" href="">
+          <FaFacebook size={28} />
+        </Link>
+        <Link className="mx-2" href="">
+          <FaTwitter size={28} />
+        </Link>
+      </div>
+      <iframe
+        className="mt-4 rounded-sm mb-7 sm:w-3/4"
+        src="https://open.spotify.com/embed/artist/7qJanCPQlXfO9DajSlDR2n?utm_source=generator"
+        height="400"
+        frameBorder="0"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+    </div>
+  );
 }
